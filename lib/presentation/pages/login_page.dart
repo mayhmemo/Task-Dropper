40d2b3f48fcee39a49748158b2ec55b65ff4d626
Mayhm <nisekoimylove@gmail.com>
2024-03-12 21:55:38 -0300

    refactor: remove gradiente de fundo

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -20,126 +20,117 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.white70, Colors.white38],
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+      
+          SizedBox(height: 30),
+          
+          Text(
+            "LOGIN",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black54,
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            SizedBox(height: 30),
-            
-            Text(
-              "LOGIN",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black54,
+      
+          SizedBox(height: 20),
+      
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30),
               ),
-            ),
-
-            SizedBox(height: 20),
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.only(left: 15.0, right: 8.0),
-                      child: Icon(Icons.lock),
-                    ),
-                    labelText: "E-mail",
-                    labelStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black54),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.only(left: 15.0, right: 8.0),
+                    child: Icon(Icons.lock),
                   ),
-                ),
-              ),
-            ),
-
-            SizedBox(height: 30),
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: TextField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: EdgeInsets.only(left: 15.0, right: 8.0),
-                      child: Icon(Icons.lock),
-                    ),
-                    labelText: "Senha",
-                    labelStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black54),
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
                   ),
-                ),
-              ),
-            ),
-
-            SizedBox(height: 30),
-
-            Container(
-              width: double.infinity,
-              height: 40.0,
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: ElevatedButton(
-                onPressed: () async {
-                  bool loginSuccess = await _localDataSource.verifyLogin(_emailController.text, _passwordController.text);
-
-                  if (loginSuccess) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  } else {
-                    final snackBar = SnackBar(content: Text('Usuário não encontrado na base de dados!'), backgroundColor: Colors.red);
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  shape: RoundedRectangleBorder(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black54),
                     borderRadius: BorderRadius.circular(30.0),
                   ),
                 ),
-                child: Text(
-                  'Entrar',
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: Colors.white,
+              ),
+            ),
+          ),
+      
+          SizedBox(height: 30),
+      
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.only(left: 15.0, right: 8.0),
+                    child: Icon(Icons.lock),
+                  ),
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black54),
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+      
+          SizedBox(height: 30),
+      
+          Container(
+            width: double.infinity,
+            height: 40.0,
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: ElevatedButton(
+              onPressed: () async {
+                bool loginSuccess = await _localDataSource.verifyLogin(_emailController.text, _passwordController.text);
+      
+                if (loginSuccess) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                } else {
+                  final snackBar = SnackBar(content: Text('Usuário não encontrado na base de dados!'), backgroundColor: Colors.red);
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              child: Text(
+                'Entrar',
+                style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       
     );
